6dd7817b45fcdb2d985b483f1a42ba956f0a84b1
Lyfectra <caylanthompson07@gmail.com>
2024-07-30 12:59:12 +1200

    Update LevelOne.gd

--- Plaformer Game/scripts/LevelOne.gd
+++ Plaformer Game/scripts/LevelOne.gd
@@ -2,15 +2,12 @@ extends Node2D
 
 @onready var ui = $Player/Camera2D/UI
 @onready var player = $Player
-@onready var kill_zone = $KillZone
 @onready var control = $UI/Control
 @onready var gem = $Gem
 @onready var gem_counter = $Player/Camera2D/UI/GemControl/GemCounter
-@onready var timer = $KillZone/Timer
 var GemCollected = 0
 
 
-	
 func _ready():
 	get_tree().paused = false
 	GemCollected = 0
